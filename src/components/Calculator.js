ccfa9b8176bec9ff4ac1274aac54de821b8a8c3e
yaredtt <yaredtekle22@gmail.com>
2023-07-13 12:23:09 +0300

    add event to calculator buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CalculatorButtons from './CalculatorButtons';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [calcData, setCalcData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonName) => {
+    setCalcData((prevState) => calculate(prevState, buttonName));
+  };
   return (
     <section>
       <div id="calculator-contaienr">
-        <div id="display">0</div>
-        <CalculatorButtons />
+        <div id="display">{calcData.next || calcData.total || '0'}</div>
+        <CalculatorButtons handleButtonClick={handleButtonClick} />
       </div>
     </section>
   );
